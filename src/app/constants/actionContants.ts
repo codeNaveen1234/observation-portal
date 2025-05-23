2b6df022691258a32d99272a84ab51aad93533be
naveen <chennamsettynaveen945@gmail.com>
2025-05-23 11:02:28 +0530

    Questionnaire package version update

---
+++ src/app/constants/actionContants.ts
@@ -0,0 +1,13 @@
+export const TITLE_MAP: Record<string, string> = {
+    observationReports: 'Observation Reports',
+    survey: 'Survey',
+    observation: 'Observation',
+    surveyReports: 'Survey Reports'
+  };
+  
+ export const DESC_KEY_MAP: Record<string, string> = {
+    survey: 'SURVEY_DESC',
+    surveyReports: 'SURVEY_DESC',
+    observation: 'OBSERVATION_LISTING_MESSAGE',
+    observationReports: 'OBSERVATION_REPORTS_DESC'
+};
