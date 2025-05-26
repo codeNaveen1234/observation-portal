0f34f86c83eeff3aeac25a09c4295b7d789813e3
naveen <chennamsettynaveen945@gmail.com>
2025-05-26 17:37:55 +0530

    Theme and headerConfiguration changes

--- src/assets/envirnoments/environment.ts
+++ src/assets/envirnoments/environment.ts
@@ -3,10 +3,12 @@ interface Environment {
     surveyBaseURL?: string;
     capabilities:'all' | 'project' | 'survey';
     isAuthBypassed: boolean;
+    showHeader:boolean;
 }
 export const environment:Environment = {
     production: true,
     surveyBaseURL: '',
     capabilities: 'survey',
-    isAuthBypassed: true
+    isAuthBypassed: true,
+    showHeader:false,
   }
